711c307064a65db9d39d0ee8545b3ee9ee5f294b
zafar26 <qutubuddinzafar2616118@gmail.com>
2022-03-03 10:40:30 +0530

    Configured

---
+++ next.config.js
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+
+const withPWA = require("next-pwa");
+
+module.exports = withPWA({
+  pwa: {
+    dest: "public",
+    register: true,
+    skipWaiting: true,
+  },
+});
