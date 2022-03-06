24b872b4717bfa3d5583e0c31e30388c6807c70e
zafar26 <qutubuddinzafar2616118@gmail.com>
2022-03-06 15:48:54 +0530

    Modified Chart CSS

--- pages/corporate/dashboard/index.tsx
+++ pages/corporate/dashboard/index.tsx
@@ -67,7 +67,7 @@ const AgentDashboard = () => {
                                 data={country}
                                 title="Country Wise "
                                 width={isMobile ? 300 : isTablet ? 600 : 900}
-                                height={isMobile ? 300 : isTablet ? 400 : 450}
+                                height={isMobile ? 300 : isTablet ? 350 : 400}
                             />
                         )}
                         {selectedButton === 1 && (
@@ -75,7 +75,7 @@ const AgentDashboard = () => {
                                 data={country}
                                 title="Country Wise "
                                 width={isMobile ? 300 : isTablet ? 600 : 900}
-                                height={isMobile ? 300 : isTablet ? 400 : 450}
+                                height={isMobile ? 300 : isTablet ? 350 : 400}
                             />
                         )}
                         {selectedButton === 2 && (
@@ -83,7 +83,7 @@ const AgentDashboard = () => {
                                 data={country}
                                 title="Country Wise "
                                 width={isMobile ? 300 : isTablet ? 600 : 900}
-                                height={isMobile ? 300 : isTablet ? 400 : 450}
+                                height={isMobile ? 300 : isTablet ? 350 : 400}
                             />
                         )}
                     </div>
